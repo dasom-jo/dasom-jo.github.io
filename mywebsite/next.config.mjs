2f51010e7935550ad9c11ee45b2216b3ecd8b71d
jodasom <jodasom0216@gmail.com>
2024-09-26 12:35:06 +0900

    배포

--- mywebsite/next.config.mjs
+++ mywebsite/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    output: 'export',
-    basePath: '/dasom-jo.github.io',
-    assetPrefix: '/dasom-jo.github.io/'
-};
+const nextConfig = {};
 
 export default nextConfig;
