1658c61b76fa2bfcbf25e182c38fe1977ee83120
jodasom <jodasom0216@gmail.com>
2024-10-01 02:28:42 +0900

    배포에러

--- mywebsite/docs/static/chunks/pages/404-f5c3fa9520a70c81.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{394:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return _(9665)}])},9665:function(n,u,_){"use strict";_.r(u);var i=_(5893);u.default=()=>(0,i.jsx)("div",{children:"해당 페이지를 찾을수없습니다 ㅠㅠ"})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=394)}),_N_E=n.O()}]);
