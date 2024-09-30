66b02d5cda2a9c88c5dee4905ea22350ba7e147e
jodasom <jodasom0216@gmail.com>
2024-10-01 01:15:57 +0900

    코드정리

--- mywebsite/src/app/containers/AboutMe.js
+++ mywebsite/src/app/containers/AboutMe.js
@@ -25,17 +25,32 @@ const AboutMe = () => {
         </div> */}
         <div className={styles.howIWork}>
           <h2 className={styles.howTitle}>how I work ?</h2>
-          <div  className={styles.howStory}>
-            <p>목표 달성도 중요하지만 <span className={styles.highlight}>일의 과정</span>에서 재미를 찾는 편이에요.</p>
+          <div className={styles.howStory}>
             <p>
-              회사와 동료에게 긍정적 영향을 주는 일은 그 자체로  <span className={styles.highlight}>동기부여</span>가
-              됩니다.
+              목표 달성도 중요하지만{" "}
+              <span className={styles.highlight}>일의 과정</span>에서 재미를
+              찾는 편이에요.
             </p>
-            <p><span className={styles.highlight}>문제해결에</span> 큰 성취감과 재미를 느낍니다.</p>
-            <p><span className={styles.highlight}>함께 정한 기한</span>은 반드시 지키려고 노력해요.</p>
-            <p><span className={styles.highlight}>파워 J 성향</span>으로 계획대로 행동합니다.</p>
             <p>
-              계획에 문제가 생겨도 당황하지 않는 <span className={styles.highlight}>강한 정신력</span>의 소유자입니다.
+              회사와 동료에게 긍정적 영향을 주는 일은 그 자체로{" "}
+              <span className={styles.highlight}>동기부여</span>가 됩니다.
+            </p>
+            <p>
+              <span className={styles.highlight}>문제해결에</span> 큰 성취감과
+              재미를 느낍니다.
+            </p>
+            <p>
+              <span className={styles.highlight}>함께 정한 기한</span>은 반드시
+              지키려고 노력해요.
+            </p>
+            <p>
+              <span className={styles.highlight}>파워 J 성향</span>으로 계획대로
+              행동합니다.
+            </p>
+            <p>
+              계획에 문제가 생겨도 당황하지 않는{" "}
+              <span className={styles.highlight}>강한 정신력</span>의
+              소유자입니다.
             </p>
           </div>
         </div>
--- mywebsite/src/app/containers/Portfoilo.js
+++ mywebsite/src/app/containers/Portfoilo.js
@@ -4,54 +4,6 @@ import styles from "./Portfolio.module.css";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 const Portfoile = () => {
-  // const frameImgRef = useRef(null); // 프레임 이미지 ref
-  // const frameLineRef = useRef(null); // 프레임 줄 ref
-
-  // //frame 확대
-  // const handleMouseEnter = () => {
-  //   if (frameImgRef.current && frameLineRef.current) {
-  //     frameImgRef.current.classList.add(styles.translate); // 프레임 다운 크기변동없음
-  //     frameLineRef.current.classList.add(styles.translateLine); // 프레임 줄 다운 크기변동없음
-
-  //     // scale이 끝난 후 translateY 적용()
-  //     setTimeout(() => {
-  //       frameImgRef.current.classList.add(styles.scale); //프레임 확대 위치변동없음
-  //       frameLineRef.current.classList.add(styles.scaleLine); //프레임줄 확대 위치변동없음
-  //     }, 800);
-  //   } else {
-  //     console.error("frameImgRef 또는 frameLineRef가 존재하지 않습니다.");
-  //   }
-  // };
-
-  // //커진 frame 원상복구 시킬때
-  // const handleMouseLeave = () => {
-  //   if(frameImgRef.current && frameLineRef.current){
-  //         // 클릭 시 scaleLeave 추가
-  //   frameImgRef.current.classList.add(styles.scaleLeave); //프레임 상승 크기 크기 변동없음
-  //   frameLineRef.current.classList.add(styles.translateLineLeave); //프레임 줄 상승 크기 변동없음
-  //   // 애니메이션 종료 후 translateLeave 추가
-  //   setTimeout(() => {
-  //     frameImgRef.current.classList.remove(styles.scaleLeave);
-  //     frameImgRef.current.classList.add(styles.translateLeave); //프레임 축소 위치 변동없음
-  //     frameLineRef.current.classList.add(styles.scaleLineLeave); //프레임 줄 축소 위치변동 없음
-  //     // 상태 초기화 (1초 후)
-  //     setTimeout(() => {
-  //       // 프레임에 선언되 클래스 이름 전부 초기화
-  //       frameImgRef.current.classList.remove(styles.translate);
-  //       frameImgRef.current.classList.remove(styles.translateLeave);
-  //       frameImgRef.current.classList.remove(styles.scale);
-  //       //프레임 줄에 선언되는 클래스 이름 전부 초기화
-  //       frameLineRef.current.classList.remove(styles.translateLine);
-  //       frameLineRef.current.classList.remove(styles.translateLineLeave);
-  //       frameLineRef.current.classList.remove(styles.scaleLine);
-  //       frameLineRef.current.classList.remove(styles.scaleLineLeave);
-  //     }, 800);
-  //   }, 800);
-  //   }else {
-  //     console.warn("에러");
-  //   }
-  // };
-
   return (
     <div className={styles.frameBox}>
       {/* 프레임에 달린 줄 */}
@@ -69,7 +21,10 @@ const Portfoile = () => {
             <div className={styles.framePortfoiloText}>
               <p>제목 : 마라pick</p>
               <p>담당 : 개인프로젝트</p>
-              <p>기술 : HTML, React.js, TypeScript, JavaScript, Scss, Next.js, MySql, recoil, GitHub</p>
+              <p>
+                기술 : HTML, React.js, TypeScript, JavaScript, Scss, Next.js,
+                MySql, recoil, GitHub
+              </p>
               <p>설명 : 마라탕을 주제로한 키오스크</p>
             </div>
           </div>
@@ -93,7 +48,10 @@ const Portfoile = () => {
             <div className={styles.framePortfoiloText}>
               <p>제목 : 어디가게</p>
               <p>담당 : openAPI,상세페이지,마이페이지</p>
-              <p>기술 : HTML,React.JS, TypeScript, JavaScript, Next.js, Mui, TailWind CSS, MySQL,ReactQurey</p>
+              <p>
+                기술 : HTML,React.JS, TypeScript, JavaScript, Next.js, Mui,
+                TailWind CSS, MySQL,ReactQurey
+              </p>
               <p>설명 : 동물허용가능 매장 지도</p>
             </div>
           </div>
