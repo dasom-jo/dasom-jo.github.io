ab96d60f59cf0ecf84bab9201e55a4b530863bd1
jodasom <jodasom0216@gmail.com>
2024-10-01 14:35:38 +0900

    css

--- mywebsite/src/app/page.js
+++ mywebsite/src/app/page.js
@@ -6,15 +6,13 @@ import Skill from "./containers/Skill";
 
 const HomePage = () => {
   return (
-    <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
-      <div className="grid-container">
-        <Contact />
-        <AboutMe />
-        <Skill />
-        <Portfoile />
-      </div>
-    </BrowserRouter>
+    <div className="grid-container">
+      <Contact/>
+      <AboutMe />
+      <Skill/>
+      <Portfoile/>
+    </div>
   );
-};
+}
 
 export default HomePage;
