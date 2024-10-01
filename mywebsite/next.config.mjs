c88156c76339bb6910600bbf58b20c0d08168895
jodasom <jodasom0216@gmail.com>
2024-10-01 13:22:49 +0900

    Updates

--- mywebsite/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: "/mywebsite",
-    trailingSlash: true,
-    assetPrefix: '/mywebsite/out'
-  };
-
-export default nextConfig;
