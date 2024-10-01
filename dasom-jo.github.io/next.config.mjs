89842cdea54fe2b25d42a1acb2edd98d057a6c3c
jodasom <jodasom0216@gmail.com>
2024-10-01 19:55:43 +0900

    배포에러

--- dasom-jo.github.io/next.config.mjs
+++ dasom-jo.github.io/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '',
-    trailingSlash: true,
-  };
+  output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/dasom-jo.github.io/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/dasom-jo.github.io" : "",
+  trailingSlash: true,
+};
 
 export default nextConfig;
