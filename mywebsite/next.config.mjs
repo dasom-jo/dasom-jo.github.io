f10c719a81b700bfd39d611eb1a764b2ad3e405f
jodasom <jodasom0216@gmail.com>
2024-10-01 10:26:25 +0900

    배포중

--- mywebsite/next.config.mjs
+++ mywebsite/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     basePath: "/dasom-jo.github.io",
+    trailingSlash: true,
   };
 
 export default nextConfig;
