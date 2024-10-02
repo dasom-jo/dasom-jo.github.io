228453c31a4077e37263ea56b12d52420ebd0164
jodasom <jodasom0216@gmail.com>
2024-10-02 19:35:17 +0900

    배포에러

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: '.next',  // 빌드 디렉토리 설정 (이 설정이 없어도 기본값은 '.next'입니다.)
+    distDir: '.next',  // 빌드 디렉토리 설정 (기본값은 '.next')
     images: {
       domains: ['www.google.com', 'tistory.com'], // 외부 도메인 허용 설정
     },
   };
 
-  module.exports = nextConfig;
+  export default nextConfig;
