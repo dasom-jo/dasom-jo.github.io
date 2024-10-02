4633fa97d19c8875ae984965f8ab00970509d818
jodasom <jodasom0216@gmail.com>
2024-10-02 19:33:46 +0900

    배포에러해결중

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    distDir: '.next',  // 빌드 디렉토리 설정 (이 설정이 없어도 기본값은 '.next'입니다.)
     images: {
-        domains: ['www.google.com', 'tistory.com'], // 외부 도메인 추가
-      },
+      domains: ['www.google.com', 'tistory.com'], // 외부 도메인 허용 설정
+    },
   };
 
-  export default nextConfig;
+  module.exports = nextConfig;
